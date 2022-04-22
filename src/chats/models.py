259a016bea84cc94ad54e796fc72f6c06dc84d77
nekit <nekitmish74@gmail.com>
2022-04-22 14:24:47 +0400

    Reworked models

--- src/chats/models.py
+++ src/chats/models.py
@@ -1,12 +1,29 @@
 from django.db import models
 
 from djangochat.models import DefaultModel
-from djangochat.models import SoftDeleteMixin
+from users.models import User
 
 
-class Chat(DefaultModel, SoftDeleteMixin):
-    participants = models.ManyToManyField(
+class Chat(DefaultModel):
+    me = models.ForeignKey(
         'users.User',
+        on_delete=models.CASCADE,
+        null=False,
         blank=False,
-        related_name='chats',
+        default=User.get_default_deleted_user,
+        related_name='chats_me',
+    )
+    companion = models.ForeignKey(
+        'users.User',
+        on_delete=models.SET(User.get_default_deleted_user),
+        null=False,
+        blank=False,
+        related_name='chats_companion',
+    )
+
+    is_archived = models.BooleanField(
+        default=False,
+    )
+    is_unread = models.BooleanField(
+        default=False,
     )
